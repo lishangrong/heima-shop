8a95f6dd3341c6d541cf9715daaef1ed00978e4a
lishangrong <lishangrong10@126.com>
2026-03-20 18:40:45 +0800

    请求拦截

--- src/pages/my/my.vue
+++ src/pages/my/my.vue
@@ -1,7 +1,15 @@
 <script setup lang="ts">
 import { useMemberStore } from '@/stores'
+import { http } from '@/utils/http'
 
 const memberStore = useMemberStore()
+const getData = async () => {
+  const res = await http<string[]>({
+    url: '/member/profile',
+    method: 'GET',
+  })
+  console.info('请求成功', res)
+}
 </script>
 
 <template>
@@ -11,6 +19,7 @@ const memberStore = useMemberStore()
       @tap="
         memberStore.setProfile({
           nickname: '黑马先锋',
+          token: '123456',
         })
       "
       size="mini"
@@ -20,6 +29,7 @@ const memberStore = useMemberStore()
       保存用户信息
     </button>
     <button @tap="memberStore.clearProfile()" size="mini" plain type="warn">清理用户信息</button>
+    <button @tap="getData" size="mini" plain type="primary">获取数据</button>
   </view>
 </template>
 
---
+++ src/utils/http.ts
@@ -0,0 +1,93 @@
+/**
+ * 添加拦截器
+ * 拦截 request 请求
+ * 拦截 uploadFile 文件上传
+ *
+ * TODO
+ * 1. 非 http 开头需要拼接地址
+ * 2. 请求超时
+ * 3. 添加小程序端请求头标识
+ * 4.添加 token 请求头
+ */
+
+import { useMemberStore } from '@/stores'
+
+const baseURL = 'https://pcapi-xiaotuxian-front-devtest.itheima.net'
+
+// 添加拦截器
+const httpInterceptor = {
+  // 拦截钱触发
+  invoke(options: UniApp.RequestOptions) {
+    // 1.非 http 开头需要拼接地址
+    if (!options.url.startsWith('http')) {
+      options.url = baseURL + options.url
+    }
+    // 2.请求超时
+    options.timeout = 10000
+    // 3.添加小程序端请求头标识
+    options.header = {
+      ...options.header,
+      'source-client': 'miniapp',
+    }
+    // 4.添加 token 请求头
+    const memberStore = useMemberStore()
+    const token = memberStore.profile?.token
+    if (token) {
+      options.header['Authorization'] = token
+    }
+  },
+}
+uni.addInterceptor('request', httpInterceptor)
+uni.addInterceptor('uploadFile', httpInterceptor)
+
+/**
+ * 请求函数
+ * @param UniApp.RequestOptions
+ * @returns Promise<any>
+ * 1. 返回 Promise 对象
+ * 2. 请求成功
+ *  - 提取核心数据 res.data
+ *  - 添加类型，支持泛型
+ * 3. 请求失败
+ *  - 网络错误 -> 提示用户换网络
+ *  - 401错误 -> 清理用户信息，跳转登录页
+ *  - 其他错误 -> 根据后端错误信息轻提示
+ * */
+
+interface HttpResponse<T> {
+  code: string
+  msg: string
+  result: T
+}
+export const http = <T>(options: UniApp.RequestOptions) => {
+  return new Promise<HttpResponse<T>>((resolve, reject) => {
+    uni.request({
+      ...options,
+      success(res) {
+        if (res.statusCode >= 200 && res.statusCode < 300) {
+          resolve(res.data as HttpResponse<T>)
+        } else if (res.statusCode === 401) {
+          const memberStore = useMemberStore()
+          memberStore.clearProfile()
+          uni.navigateTo({
+            url: '/pages/login/login',
+          })
+          reject(res)
+        } else {
+          uni.showToast({
+            title: (res.data as HttpResponse<T>).msg || '请求失败',
+            icon: 'none',
+          })
+          reject(res)
+        }
+      },
+      fail(err) {
+        uni.showToast({
+          title: '网络错误，换个网络',
+          icon: 'none',
+        })
+        reject(err)
+      },
+    })
+  })
+}
