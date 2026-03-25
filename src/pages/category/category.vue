ba6e562ccdd34e8270425c451fa30aed2dcf054d
lishangrong <lishangrong10@126.com>
2026-03-25 10:22:24 +0800

    一级分类获取与渲染

--- src/pages/category/category.vue
+++ src/pages/category/category.vue
@@ -2,7 +2,10 @@
 import { ref } from 'vue'
 import { getHomeBannerAPI } from '@/services/home'
 import type { BannerItem } from '@/types/home'
+import type { CategoryTopItem } from '@/types/category'
+
 import { onLoad } from '@dcloudio/uni-app'
+import { getCategoryTopAPI } from '@/services/category'
 
 // 获取轮播图的数据
 const bannerList = ref<BannerItem[]>([])
@@ -10,10 +13,18 @@ const getBannerData = async () => {
   const res = await getHomeBannerAPI(2)
   bannerList.value = res.result
 }
+// 获取分类列表-小程序
+const categoryTopList = ref<CategoryTopItem[]>([])
+const activeIndex = ref(0)
+const getCategoryTopData = async () => {
+  const res = await getCategoryTopAPI()
+  categoryTopList.value = res.result
+}
 
 // 页面加载
 onLoad(() => {
   getBannerData()
+  getCategoryTopData()
 })
 </script>
 
@@ -29,8 +40,14 @@ onLoad(() => {
     <view class="categories">
       <!-- 左侧：一级分类 -->
       <scroll-view class="primary" scroll-y>
-        <view v-for="(item, index) in 10" :key="item" class="item" :class="{ active: index === 0 }">
-          <text class="name"> 居家 </text>
+        <view
+          v-for="(item, index) in categoryTopList"
+          :key="item.id"
+          class="item"
+          :class="{ active: activeIndex === index }"
+          @tap="activeIndex = index"
+        >
+          <text class="name"> {{ item.name }} </text>
         </view>
       </scroll-view>
       <!-- 右侧：二级分类 -->
