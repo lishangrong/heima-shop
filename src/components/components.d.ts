6eade2ff8d7faae0641d5a71b830d38c4d35f864
lishangrong <lishangrong10@126.com>
2026-03-26 18:37:56 +0800

    去掉重复定义

--- src/components/components.d.ts
+++
@@ -1,9 +0,0 @@
-import XtxSwiper from './XtxSwiper.vue'
-import XtxGuess from './XtxGuess.vue'
-
-declare module '@vue/runtime-core' {
-  export interface GlobalComponents {
-    XtxSwiper: typeof XtxSwiper
-    XtxGuess: typeof XtxGuess
-  }
-}
